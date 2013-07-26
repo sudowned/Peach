b611263bfb99425655d5e240fbe34b7c92bf5389
Winfield Trail <dev@sudowned.com>
2013-07-26 16:57:55 -0400

    Improved test code for prettiness

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?
 include('peach.php');
 $Peach = new Peach();
-$String = 'lol! look out below, guys';
-echo ($Peach->String($String)->Replace("LOL", "i am amused", Stems::CaseInsensitive));
+$String = $Peach->String('lol! look out below, guys');
+echo ($String->Replace("LOL", "i am amused", Stems::CaseInsensitive));
